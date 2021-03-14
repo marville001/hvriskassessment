465fbc2c0380b482dd29a2217cd961240cbd33e3
marville001 <mwangimartin1904@gmail.com>
2021-03-14 16:19:24 +0300

    Update

--- src/_actions/userActions.js
+++ src/_actions/userActions.js
@@ -7,29 +7,28 @@ import {
 import Axios from "axios";
 
 // Online db
-const api ="https://hvriskassessment.herokuapp.com"
+const api = "https://hvriskassessment.herokuapp.com";
 
 // offline db
 // const api ="http://localhost:5005"
 
-// 
+//
 const userLogin = (user) => async (dispatch) => {
   dispatch({ type: USER_SIGNIN_REQUEST, payload: user });
   try {
-    const { data } = await Axios.post(api+"/api/auth", user);
+    const { data } = await Axios.post(api + "/api/auth", user);
     localStorage.setItem("token", data.token);
     dispatch(loginUser(data.user));
   } catch (error) {
     localStorage.removeItem("token");
     dispatch({ type: USER_SIGNIN_FAILED, error: error.response.data.message });
   }
-
 };
 
 const getProfileFetch = () => (dispatch) => {
   const token = localStorage.token;
   if (token) {
-    fetch(api+"/api/users/me", {
+    fetch(api + "/api/users/me", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -37,10 +36,12 @@ const getProfileFetch = () => (dispatch) => {
       },
     })
       .then((resp) => resp.json())
-      .then((data) => {        
-          dispatch(loginUser(data.user));
+      .then((data) => {
+        dispatch(loginUser(data.user));
       })
-      .catch((error) => {});
+      .catch((error) => {
+        localStorage.removeItem("token");
+      });
   }
 };
 const loginUser = (user) => ({
