4168a8267edc03177fa39784690411d60240cb3f
marville001 <mwangimartin1904@gmail.com>
2021-03-15 06:26:42 +0300

    Update

--- src/components/Session/HVDamageAssessment.jsx
+++ src/components/Session/HVDamageAssessment.jsx
@@ -1,12 +1,110 @@
-import React from 'react'
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import HazardQuestion from "../HazardQuestion";
 
 const HVDamageAssessment = () => {
-    return (
-        <div>
-            <h3>HV Damage Assessment</h3>
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h3 className="text-center p-2">HV Damage Assessment</h3>
+      <Container fluid className="si_container">
+        <HazardQuestion
+          quiz={1}
+          dispatch={dispatch}
+          question={"Is the Battery or Battery storage Area physically damaged"}
+          action={() => {}}
+          // _id={hazard._id}
+          // what={hazard.onfire}
+          state={"onfire"}
+        />
+
+        <HazardQuestion
+          quiz={2}
+          dispatch={dispatch}
+          question={"Is the Battery or Storage Area leaking fluid"}
+          action={() => {}}
+          // _id={hazard._id}
+          // what={hazard.onfire}
+          state={"onfire"}
+        />
+
+        <HazardQuestion
+          quiz={3}
+          dispatch={dispatch}
+          question={"Is there any smoke present in the battery area"}
+          action={() => {}}
+          // _id={hazard._id}
+          // what={hazard.onfire}
+          state={"onfire"}
+        />
+
+        <HazardQuestion
+          quiz={4}
+          dispatch={dispatch}
+          question={"Is there a sweet odor present"}
+          action={() => {}}
+          // _id={hazard._id}
+          // what={hazard.onfire}
+          state={"onfire"}
+        />
+
+        <HazardQuestion
+          quiz={5}
+          dispatch={dispatch}
+          question={"Is there any damage to the battery components themselves"}
+          action={() => {}}
+          // _id={hazard._id}
+          // what={hazard.onfire}
+          state={"onfire"}
+        />
+
+        <HazardQuestion
+          quiz={6}
+          dispatch={dispatch}
+          question={"Are the batteries separated from the housing"}
+          action={() => {}}
+          // _id={hazard._id}
+          // what={hazard.onfire}
+          state={"onfire"}
+        />
+
+        <HazardQuestion
+          quiz={7}
+          dispatch={dispatch}
+          question={" Are there any indications of arcing or electrical damage"}
+          action={() => {}}
+          // _id={hazard._id}
+          // what={hazard.onfire}
+          state={"onfire"}
+        />
+
+        <div className="item_container">
+          <div className="q_container">
+            {/* <div className="q_num">{1}</div> */}
+            <div className="q_text">
+              Identify the HV cable and tell me if there is any damage to it
+              from source to source
+            </div>
+            <div className="q_buttons">
+              <Button className="q_button" onClick={() => {}}>
+                YES
+              </Button>
+              <Button className="q_button" onClick={() => {}}>
+                NO
+              </Button>
+            </div>
+          </div>
         </div>
-    )
-}
 
-export default HVDamageAssessment
+        <div className="d-flex-center py-5">
+          <Button onClick={() => {}} variant="success">
+            Finish Session
+          </Button>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
+export default HVDamageAssessment;
--- src/components/Session/VehicleDamageAssessment.jsx
+++ src/components/Session/VehicleDamageAssessment.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import {
+  Accordion,
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  Row,
+} from "react-bootstrap";
 
-const VehicleDamageAssessment = () => {
+const VehicleDamageAssessment = ({ sessionId,activeStep,setActiveStep }) => {
   return (
     <div>
       <h3 className="text-center p-2">Vehicle Damage Assessment</h3>
@@ -25,7 +33,6 @@ const VehicleDamageAssessment = () => {
           </div>
         </div>
 
-
         <div className="item_container">
           <div className="q_container">
             <div className="q_text">
@@ -45,12 +52,11 @@ const VehicleDamageAssessment = () => {
           </div>
         </div>
 
-
         <div className="item_container">
           <div className="q_container">
             <Form.Group as={Row}>
               <Form.Label className="q_text" as="legend" column sm={12}>
-              Please Rate the Severity of the Damage
+                Please Rate the Severity of the Damage
               </Form.Label>
               <Col sm={10}>
                 <Form.Check
@@ -78,6 +84,115 @@ const VehicleDamageAssessment = () => {
             <span>+</span> Add Note
           </p>
         </div>
+
+        <Accordion>
+          <Card>
+            <Card.Header>
+              <div className="q_container">
+                <div className="q_text">
+                  <div className="ask">*</div>Did the airbags Deploy{" "}
+                  <span>?</span>
+                </div>
+                <div className="q_buttons">
+                  <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                    <Button className="q_button" onClick={() => {}}>
+                      YES
+                    </Button>
+                  </Accordion.Toggle>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                    <Button className="q_button" onClick={() => {}}>
+                      NO
+                    </Button>
+                  </Accordion.Toggle>
+                </div>
+              </div>
+            </Card.Header>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body className="item_container">
+                <div className="q_container">
+                  <div className="q_text">
+                    Which airbag? <span>?</span>
+                  </div>
+                  <div className="q_bukttons">
+                    <Form.Control as="select" custom defaultValue="Choose...">
+                      <option>Choose...</option>
+                      <option>Front</option>
+                      <option>Drivers side</option>
+                      <option>Passenger Side</option>
+                      <option>Rear</option>
+                      <option>Undercarriage</option>
+                      <option>Top</option>
+                    </Form.Control>
+                  </div>
+                </div>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+
+        <Accordion>
+          <Card>
+            <Card.Header>
+              <div className="q_container">
+                <div className="q_text">
+                  <div className="ask">*</div>Was the vehicle submerged or
+                  exposed to high volumes of water <span>?</span>
+                </div>
+                <div className="q_buttons">
+                  <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                    <Button className="q_button" onClick={() => {}}>
+                      YES
+                    </Button>
+                  </Accordion.Toggle>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                    <Button className="q_button" onClick={() => {}}>
+                      NO
+                    </Button>
+                  </Accordion.Toggle>
+                </div>
+              </div>
+            </Card.Header>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body className="item_container">
+                <div className="q_container">
+                  <div className="q_text">
+                    Is the battery area flooded <span>?</span>
+                  </div>
+                  <div className="q_buttons">
+                    <Button className="q_button" onClick={() => {}}>
+                      YES
+                    </Button>
+                    <Button className="q_button" onClick={() => {}}>
+                      NO
+                    </Button>
+                  </div>
+                </div>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+
+        <div className="item_container">
+          <div className="q_container">
+            {/* <div className="q_num">{1}</div> */}
+            <div className="q_text">
+              Was the vehicle on fire or is there any evidence of fire damage{" "}
+              <span>?</span>
+            </div>
+            <div className="q_buttons">
+              <Button className="q_button" onClick={() => {}}>
+                YES
+              </Button>
+              <Button className="q_button" onClick={() => {}}>
+                NO
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        <div className="d-flex-center py-5">
+            <Button onClick={()=>setActiveStep(activeStep+1)} variant="success">Next</Button>
+          </div>
       </Container>
     </div>
   );
